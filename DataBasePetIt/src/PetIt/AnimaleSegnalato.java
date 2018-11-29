00dbf684c6a1e2ca5eb6c819ee0ad18edbe5b68a
Andrea <andrearovatti05@gmail.com>
2018-11-29 17:51:44 +0100

    sistemazione

---
+++ DataBasePetIt/src/PetIt/AnimaleSegnalato.java
@@ -0,0 +1,101 @@
+package PetIt;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class AnimaleSegnalato {
+	@Id
+	private String idAnimale;
+	@ManyToOne
+	private Razza razza;
+
+	private String colorePelo;
+	private String tipoPelo;
+	private String taglia;
+	private String statoFisico;
+	private String statoMentale;
+	private String commento;
+	
+	@ManyToMany(mappedBy="animaliSegnalati")
+	private List<PosizioniRilevamentiSegnalazioni> posizioni;
+
+	public String getIdAnimale() {
+		return idAnimale;
+	}
+
+	public void setIdAnimale(String idAnimale) {
+		this.idAnimale = idAnimale;
+	}
+
+	public Razza getRazza() {
+		return razza;
+	}
+
+	public void setRazza(Razza razza) {
+		this.razza = razza;
+	}
+
+	public String getColorePelo() {
+		return colorePelo;
+	}
+
+	public void setColorePelo(String colorePelo) {
+		this.colorePelo = colorePelo;
+	}
+
+	public String getTipoPelo() {
+		return tipoPelo;
+	}
+
+	public void setTipoPelo(String tipoPelo) {
+		this.tipoPelo = tipoPelo;
+	}
+
+	public String getTaglia() {
+		return taglia;
+	}
+
+	public void setTaglia(String taglia) {
+		this.taglia = taglia;
+	}
+
+	public String getStatoFisico() {
+		return statoFisico;
+	}
+
+	public void setStatoFisico(String statoFisico) {
+		this.statoFisico = statoFisico;
+	}
+
+	public String getStatoMentale() {
+		return statoMentale;
+	}
+
+	public void setStatoMentale(String statoMentale) {
+		this.statoMentale = statoMentale;
+	}
+
+	public String getCommento() {
+		return commento;
+	}
+
+	public void setCommento(String commento) {
+		this.commento = commento;
+	}
+
+	public List<PosizioniRilevamentiSegnalazioni> getPosizioni() {
+		return posizioni;
+	}
+
+	public void setPosizioni(List<PosizioniRilevamentiSegnalazioni> posizioni) {
+		this.posizioni = posizioni;
+	}
+
+	
+
+}
