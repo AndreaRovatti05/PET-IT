140faddca0c3282da24eb146dbc14d23a3bde815
Andrea <andrearovatti05@gmail.com>
2018-12-05 16:29:07 +0100

    fine

--- PetItDataBase/src/main/java/business/SpecieManager.java
+++ PetItDataBase/src/main/java/business/SpecieManager.java
@@ -1,5 +1,30 @@
 package business;
 
-public class SpecieManager {
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import javax.persistence.EntityManager;
+
+import modello.AnimaleSegnalato;
+import modello.Specie;
+import utility.Programma;
+
+public class SpecieManager {
+private static Logger log = Logger.getLogger("petit-business");
+	
+	public static void aggiungiSpecie(Specie sp) {
+		EntityManager em = Programma.getEm();
+		AnimaleSegnalato aDb = null;
+		if(sp.getIdSpecie() != null) {
+			aDb = em.find(AnimaleSegnalato.class, sp.getIdSpecie());
+		}
+		if (aDb == null) {
+			em.getTransaction().begin();
+			em.persist(sp); 
+			em.getTransaction().commit();
+			log.log(Level.INFO, "aggiunta presenza");
+		} else {
+			log.log(Level.WARNING, "presenza esiste già");
+		}
+	}
 }
--- PetItDataBase/src/main/java/business/UtenteRegistratoManager.java
+++ PetItDataBase/src/main/java/business/UtenteRegistratoManager.java
@@ -1,5 +1,30 @@
 package business;
 
-public class UtenteRegistratoManager {
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import javax.persistence.EntityManager;
+
+import modello.AnimaleSegnalato;
+import modello.UtenteRegistrato;
+import utility.Programma;
+
+public class UtenteRegistratoManager {
+private static Logger log = Logger.getLogger("petit-business");
+	
+	public static void aggiungiUtenteRegistrato(UtenteRegistrato u) {
+		EntityManager em = Programma.getEm();
+		AnimaleSegnalato aDb = null;
+		if(u.getIdUtente() != null) {
+			aDb = em.find(AnimaleSegnalato.class, u.getIdUtente());
+		}
+		if (aDb == null) {
+			em.getTransaction().begin();
+			em.persist(u); 
+			em.getTransaction().commit();
+			log.log(Level.INFO, "aggiunta presenza");
+		} else {
+			log.log(Level.WARNING, "presenza esiste già");
+		}
+	}
 }
